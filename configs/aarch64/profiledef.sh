4965b797ef54a6c9f579e97fc1d9d78d67577bc0
IComplainInComments <optachan@outlook.com>
2023-05-05 01:15:16 -0400

    Updated, Supports Systemd-Boot only for now.

--- configs/aarch64/profiledef.sh
+++ configs/aarch64/profiledef.sh
@@ -8,11 +8,11 @@ iso_application="Arch Linux Arm baseline"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('uefi-aarch64.grub.esp' 'uefi-aarch64.grub.eltorito')
+bootmodes=('uefi-aarch64.systemd-boot.esp' 'uefi-aarch64.systemd-boot.eltorito')
 arch="aarch64"
 pacman_conf="pacman.conf"
-airootfs_image_type="erofs"
-airootfs_image_tool_options=('-zlzma,109' -E 'ztailpacking,fragments,dedupe')
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'arm' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
 )
